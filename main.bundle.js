19efcef6217d31e62f38cacd104ae4ee5ff511d3
Victor Vysotskiy <vikont7@yahoo.com>
2022-10-31 17:00:02 +0300

    Commit

--- main.bundle.js
+++ main.bundle.js
@@ -56,16 +56,11 @@ var App = function App() {
       timeout: 300,
       children: (0, jsx_runtime_1.jsxs)(react_router_dom_1.Routes, {
         children: [(0, jsx_runtime_1.jsx)(react_router_dom_1.Route, {
-          path: "/",
+          path: "/*",
           element: (0, jsx_runtime_1.jsx)(MainMenu_1["default"], {})
         }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, {
           path: "/contact_form",
           element: (0, jsx_runtime_1.jsx)(ContactForm_1["default"], {})
-        }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, {
-          path: "*",
-          element: (0, jsx_runtime_1.jsx)(react_router_dom_1.Navigate, {
-            to: "/"
-          })
         })]
       })
     }, location.pathname)
@@ -176,12 +171,6 @@ exports["default"] = TextInput;
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var __importDefault = this && this.__importDefault || function (mod) {
   return mod && mod.__esModule ? mod : {
     "default": mod
@@ -200,14 +189,10 @@ var tgBackButton_1 = __webpack_require__(557);
 var tgMainButton_1 = __webpack_require__(285);
 __webpack_require__(400);
 var ContactForm = function ContactForm() {
-  var _ref = (0, react_1.useState)(0),
-    _ref2 = _slicedToArray(_ref, 2),
-    counter = _ref2[0],
-    setCounter = _ref2[1];
   var navigate = (0, react_router_dom_1.useNavigate)();
-  var _ref3 = (0, react_hook_form_1.useForm)(),
-    register = _ref3.register,
-    handleSubmit = _ref3.handleSubmit;
+  var _ref = (0, react_hook_form_1.useForm)(),
+    register = _ref.register,
+    handleSubmit = _ref.handleSubmit;
   var formRef = (0, react_1.useRef)(null);
   var navigateBack = (0, react_1.useCallback)(function () {
     navigate('/');
