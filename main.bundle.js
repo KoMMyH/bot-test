d4c9e62399d3ff53b4546a5de1bdb5f300fe1c3f
Victor Vysotskiy <vikont7@yahoo.com>
2022-10-31 16:36:56 +0300

    Commit

--- main.bundle.js
+++ main.bundle.js
@@ -56,11 +56,16 @@ var App = function App() {
       timeout: 300,
       children: (0, jsx_runtime_1.jsxs)(react_router_dom_1.Routes, {
         children: [(0, jsx_runtime_1.jsx)(react_router_dom_1.Route, {
-          path: "/*",
+          path: "/",
           element: (0, jsx_runtime_1.jsx)(MainMenu_1["default"], {})
         }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, {
           path: "/contact_form",
           element: (0, jsx_runtime_1.jsx)(ContactForm_1["default"], {})
+        }), (0, jsx_runtime_1.jsx)(react_router_dom_1.Route, {
+          path: "*",
+          element: (0, jsx_runtime_1.jsx)(react_router_dom_1.Navigate, {
+            to: "/"
+          })
         })]
       })
     }, location.pathname)
@@ -434,7 +439,7 @@ var useTgMainButton = function useTgMainButton(_ref) {
     return function () {
       tgWebApp_1.WebApp.MainButton.hide();
     };
-  });
+  }, []);
   (0, react_1.useEffect)(function () {
     onClickListener(onClick);
     return function () {
@@ -8162,18 +8167,6 @@ module.exports = __webpack_require__.p + "d544ddcf6c351d3e93b1.png";
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -8192,22 +8185,7 @@ module.exports = __webpack_require__.p + "d544ddcf6c351d3e93b1.png";
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 		__webpack_require__.p = "/";
 /******/ 	})();
 /******/ 	
 /************************************************************************/
